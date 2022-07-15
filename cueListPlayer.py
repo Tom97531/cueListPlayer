e7bf7868f9082eecc073c044aba4c5054c03ca12
Thomas Piquet <tpiquet@sat.qc.ca>
2022-07-15 14:05:42 -0400

    Adds 2 lines of cues + updates cue list

--- cueListPlayer.py
+++ cueListPlayer.py
@@ -14,12 +14,14 @@ def Refresher():
 
     # switch to red 10s before
     if current_time - start_time > next_cue_time - 10:
-        next.configure(fg="red")
+        next.configure(bg="red")
 
     if current_time - start_time > next_cue_time:
         current.configure(text=f"{cues.cues[current_cue][0]} {cues.cues[current_cue][1]}")
         next.configure(text=f"{cues.cues[current_cue+1][0]} {cues.cues[current_cue+1][1]}",
-                        fg="black")
+                        bg="white")
+        next_2.configure(text=f"{cues.cues[current_cue+2][0]} {cues.cues[current_cue+2][1]}")
+        next_3.configure(text=f"{cues.cues[current_cue+3][0]} {cues.cues[current_cue+3][1]}")
         current_cue = current_cue + 1
 
     root.after(1000, Refresher)
@@ -27,23 +29,37 @@ def Refresher():
 if __name__ == "__main__":
     root=tk.Tk()
     root.configure(background='black')
-    
+
     start_time = time.time()
     current_cue = 0
 
-    current = tk.Label(root, 
-        text=f"{cues.cues[current_cue][0]} {cues.cues[current_cue][1]}", 
-        font=("Helvetica", 100), 
-        background="yellow", 
+    current = tk.Label(root,
+        text=f"{cues.cues[current_cue][0]} {cues.cues[current_cue][1]}",
+        font=("Helvetica", 100),
+        background="yellow",
         width=100)
     current.pack()
-    
-    next = tk.Label(root, 
-            text=f"{cues.cues[current_cue+1][0]} {cues.cues[current_cue+1][1]}", 
-            font=("Helvetica", 100), 
-            background="white", 
+
+    next = tk.Label(root,
+            text=f"{cues.cues[current_cue+1][0]} {cues.cues[current_cue+1][1]}",
+            font=("Helvetica", 100),
+            background="white",
             width=100)
     next.pack()
+
+    next_2 = tk.Label(root,
+            text=f"{cues.cues[current_cue+2][0]} {cues.cues[current_cue+2][1]}",
+            font=("Helvetica", 100),
+            background="white",
+            width=100)
+    next_2.pack()
+
+    next_3 = tk.Label(root,
+            text=f"{cues.cues[current_cue+3][0]} {cues.cues[current_cue+3][1]}",
+            font=("Helvetica", 100),
+            background="white",
+            width=100)
+    next_3.pack()
 
     Refresher()
     root.mainloop()
